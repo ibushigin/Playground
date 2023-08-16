ddd4f7375bca70c31bebf4fc5528441a5030e04f
Jean L <jean.lalanne@eulerts.com>
2023-08-16 17:29:20 +0200

    loader

---
+++ src/commonComponents/LoaderPage.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="loader-container">
+    <div class="loader">
+      <div class="ball magic-swamp-bg"></div>
+      <div class="ball magic-mountain-bg"></div>
+      <div class="ball magic-forest-bg"></div>
+      <div class="ball magic-plain-bg"></div>
+      <div class="ball magic-island-bg"></div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.loader-container {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+}
+.loader {
+  width: 500px;
+  height: 500px;
+  position: relative;
+  .ball {
+    border: 6px black solid;
+    border-radius: 50%;
+    height: 15%;
+    width: 15%;
+    padding: 5px;
+    position: absolute;
+    transform: translate(-50%, -50%) scale(1);
+    animation: pulse 2s infinite;
+    &:first-child {
+      top: 15%;
+      left: 50%;
+    }
+    &:nth-child(2) {
+      left: 85%;
+      top: 45%;
+    }
+    &:nth-child(3) {
+      top: 85%;
+      left: 75%;
+    }
+    &:nth-child(4) {
+      top: 85%;
+      left: 25%;
+    }
+    &:nth-child(5) {
+      left: 15%;
+      top: 45%;
+    }
+  }
+}
+
+.icon {
+  object-fit: contain;
+  width: 100%;
+  height: 100%;
+}
+@keyframes pulse {
+  0% {
+    transform: translate(-50%, -50%) scale(0.95);
+    box-shadow: 0 0 0 0 rgba(0, 0, 0, 0.7);
+  }
+
+  70% {
+    transform: translate(-50%, -50%) scale(1);
+    box-shadow: 0 0 0 10px rgba(0, 0, 0, 0);
+  }
+
+  100% {
+    transform: translate(-50%, -50%) scale(0.95);
+    box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,70 @@
+<template>
+  <Loader v-if="cardsLoading"></Loader>
+  <div class="cards-container mt-10" v-if="!cardsLoading">
+    <div v-for="(card, key) in cards" :key="key" class="card-container">
+      <div class="card" @pointermove="interact" @mouseout="interactEnd">
+        <img :src="card.imageUrl" :alt="card.name" />
+      </div>
+    </div>
+  </div>
+</template>
 <script setup lang="ts">
 import axios from 'axios'
-import { ref, onMounted } from 'vue'
-import type { ICard } from '/interface/ICards'
+import { ref, onMounted, type Ref } from 'vue'
+import type { ICard } from '@/interface/ICards'
+import Loader from '@/commonComponents/LoaderPage.vue'
 
-const cards = ref()
+const cards: Ref<ICard[]> = ref([])
+const cardsLoading: Ref<boolean> = ref(true)
+const imagesLoaded = ref(0)
+const interact = (e: MouseEvent) => {
+  const target = e.target as HTMLElement
+  const rect = target.getBoundingClientRect()
+  const absolute = {
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top
+  }
+}
+const interactEnd = () => {}
+
 const fetchCards = async () => {
   try {
     const response = await axios.get('https://api.magicthegathering.io/v1/cards')
-    cards.value = response.data.cards
-    console.log(cards.value)
+    cards.value = response.data.cards.filter((card: ICard) => 'imageUrl' in card)
+    loadImages()
   } catch (error) {
     console.error(error)
   }
+}
+const loadImages = () => {
+  cards.value.forEach((obj) => {
+    const image = new Image()
+    image.src = obj.imageUrl
+    image.onload = () => {
+      imagesLoaded.value++
+      if (imagesLoaded.value === cards.value.length) {
+        cardsLoading.value = false
+      }
+    }
+  })
 }
 onMounted(() => {
   fetchCards()
 })
 </script>
 
-<template>
-  <div v-for="(card key) in cards" :key="key">
-    <img v-if="card.imageUrl" :src="card.imageUrl" :alt="card.name" />
-  </div>
-</template>
+<style scoped lang="scss">
+.cards-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  row-gap: 1em;
+  .card-container {
+    justify-self: center;
+  }
+  .card {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+</style>
